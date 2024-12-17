5a629bfa0358734bf56b1f009fe017cfa03b863f
Aira Sakuranomiya <aira@aira.cafe>
2024-12-18 01:03:02 +0800

    feat: switch to official hosted Inter fonts

--- app.vue
+++ app.vue
@@ -81,9 +81,8 @@
 			{ rel: "alternate", href: `${homepage}fr`, hreflang: "fr" },
 			{ rel: "alternate", href: `${homepage}yue`, hreflang: "zh-yue" },
 			{ rel: "alternate", href: `${homepage}yue`, hreflang: "yue" },
-			{ rel: "preconnect", href: "https://fonts.googleapis.com" },
-			{ rel: "preconnect", href: "https://fonts.gstatic.com", crossorigin: "" },
-			{ rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" },
+			{ rel: "preconnect", href: "https://rsms.me/" },
+			{ rel: "stylesheet", href: "https://rsms.me/inter/inter.css" },
 		],
 	});
 
